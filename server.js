a73d54427bf3042f09a5ec6810c2cfe65ea1f5e9
kovacsadii23 <kovacsadii@proton.me>
2025-03-20 07:09:00 +0100

    Added MongoDB

--- server.js
+++ server.js
@@ -3,14 +3,31 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+mongoose.connect('mongodb+srv://<kovacs23adii>:<KovAdrAdmin23>@vizoop.mongodb.net/javafiles', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Define a schema for Java files
+const JavaFileSchema = new mongoose.Schema({
+    fileName: String,
+    content: String
+});
+
+const JavaFile = mongoose.model('JavaFile', JavaFileSchema);
 
 function sendJavaFile(res, filePath) {
     fs.readFile(filePath, 'utf8', (err, data) => {
