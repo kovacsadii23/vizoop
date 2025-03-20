44c13ff7f58ebd329c4f673ec6c5704888b05909
kovacsadii23 <kovacsadii@proton.me>
2025-03-20 07:20:28 +0100

    update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const fs = require('fs'); 
 const os = require('os');
 const path = require('path');
@@ -15,11 +16,12 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-mongoose.connect('mongodb+srv://<kovacs23adii>:<KovAdrAdmin23>@vizoop.mongodb.net/javafiles', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+})
+.then(() => console.log('MongoDB connected successfully!'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const JavaFileSchema = new mongoose.Schema({
     fileName: String,
